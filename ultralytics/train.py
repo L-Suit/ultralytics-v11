5c7eedddcc76f8b15ef379afb0a1f20ac93d13be
lsh <li78789327@163.com>
2025-03-04 14:51:01 +0800

    fix ADown+SGD

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -3,14 +3,14 @@ from ultralytics import YOLO, RTDETR
 # Load a COCO-pretrained YOLO11n model
 
 if __name__ == '__main__':
-    #model = YOLO("./cfg/models/11/yolo11-ADown.yaml")
+    model = YOLO("./cfg/models/11/yolo11-ADown.yaml")
     #model = YOLO("/root/ultralytics-v11/ultralytics/runs/detect/yolov11-ADown_for31V2_epo200_lr0.001_16_AdamW_wk6_wd0.0005_sz544_/weights/last.pt")
-    model = RTDETR(r'./cfg/models/rt-detr/rtdetr-l.yaml')
+    #model = RTDETR(r'./cfg/models/rt-detr/rtdetr-l.yaml')
     imgsz = 544
     epoch = 200
     batch = 16
-    optimizer = 'AdamW'
-    lr0 = 0.001
+    optimizer = 'SGD'
+    lr0 = 0.01
     patience = 0
     weight_decay = 0.0005
     workers = 6
@@ -37,6 +37,6 @@ if __name__ == '__main__':
                 resume=False, # 如过想续训,此处设置true，model不用.yaml改为last.pt的位置
                 amp=True,  # 如果出现训练损失为Nan可以关闭amp
                 # half=True,
-                project='/root/autodl-tmp/detect/rtdetr',
-                name=f'rtdetr-l_for31V2_epo{epoch}_lr{lr0}_{batch}_{optimizer}_wd{weight_decay}_sz{imgsz}_',
+                project='/root/autodl-tmp/detect',
+                name=f'yolo11-ADown_for31V2_epo{epoch}_lr{lr0}_{batch}_{optimizer}_wd{weight_decay}_sz{imgsz}_',
                 )
